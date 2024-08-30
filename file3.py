a9d7d5f408541261d0181964995a93cefef8299c
bhrath <dasari@gmail.com>
2024-08-30 09:34:45 +0000

    sample text

---
+++ file3.py
@@ -0,0 +1,26 @@
+print("Simple program for testing")
+print("sudo yum install -y java-7")
+print("sudo yum install -y java-17")
+print("java version")
+print("java --version")
+print("sudo wget -O /etc/yum.repos.d/jenkins.rep_https://pkg.jenkins.io/redhat/jenkins.repo")
+print("sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key")
+print("sudo dnf upgrade")
+print("Add required dependencies for the jenkins package")
+    #9  sudo dnf install fontconfig java-17-openjdk,
+  # 10  sudo dnf install jenkins,
+   #11  jenkins --version,
+   #12  sudo systemctl start jenkins")
+#print(" cd /var,
+ #  18  ls,
+  # 19  cd lib,
+   #20  ls,
+   #21  cd jenkins/,
+   #22  ls,
+   #23  sudo chmod 777 secrets,
+   #24  cd secrets/,
+   #25  ls,
+   #26  sudo chmod 777 initialAdminPassword,
+   #27  ls,
+   #28  cat initialAdminPassword")
+
